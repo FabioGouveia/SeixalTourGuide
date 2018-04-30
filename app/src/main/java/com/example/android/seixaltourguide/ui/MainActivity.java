373f4debcc2afca89561a7491894fda5758d4c9c
Fábio Gouveia <hpgroods@gmail.com>
2018-04-30 09:15:21 +0100

    feat: add image animation
    Signed-off-by: Fábio Gouveia <hpgroods@gmail.com>

--- app/src/main/java/com/example/android/seixaltourguide/ui/MainActivity.java
+++ app/src/main/java/com/example/android/seixaltourguide/ui/MainActivity.java
@@ -1,14 +1,16 @@
 package com.example.android.seixaltourguide.ui;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
-import android.support.v4.app.ListFragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
 
 import com.example.android.seixaltourguide.R;
@@ -18,6 +20,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     private DrawerLayout drawer;
     private ImageView actionBarImage;
+    private Animation actionBarImageAnimation;
+    private int imageToEnterResourceId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +41,9 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        actionBarImageAnimation = AnimationUtils.loadAnimation(this, R.anim.fadeout_action_bar_image);
+        actionBarImageAnimation.setAnimationListener(new ActionBarImageAnimationListener());
     }
 
     @Override
@@ -50,20 +57,29 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
         PlacesListFragment placesListFragment = (PlacesListFragment) getSupportFragmentManager().findFragmentById(R.id.places_list_fragment);
         int id = item.getItemId();
 
         if (id == R.id.nav_attractions) {
-            actionBarImage.setImageResource(R.drawable.header_attractions);
+
+            actionBarImage.startAnimation(actionBarImageAnimation);
+            imageToEnterResourceId = R.drawable.header_attractions;
+
             placesListFragment.changeDataSet(Place.PLACE_TYPE_ATTRACTION);
         } else if (id == R.id.nav_events) {
-            actionBarImage.setImageResource(R.drawable.header_events);
+
+            actionBarImage.startAnimation(actionBarImageAnimation);
+            imageToEnterResourceId = R.drawable.header_events;
         } else if (id == R.id.nav_public_places) {
-            actionBarImage.setImageResource(R.drawable.header_public_places);
+
+            actionBarImage.startAnimation(actionBarImageAnimation);
+            imageToEnterResourceId = R.drawable.header_public_places;
         } else if (id == R.id.nav_restaurants) {
-            actionBarImage.setImageResource(R.drawable.header_restaurants);
+
+            actionBarImage.startAnimation(actionBarImageAnimation);
+            imageToEnterResourceId = R.drawable.header_restaurants;
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
@@ -72,5 +88,21 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
         drawer.closeDrawer(GravityCompat.START);
         return true;
+    }
+
+    private class ActionBarImageAnimationListener implements Animation.AnimationListener {
+
+        @Override
+        public void onAnimationStart(Animation animation) {
+        }
+
+        @Override
+        public void onAnimationEnd(Animation animation) {
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+            actionBarImage.setImageResource(imageToEnterResourceId);
+        }
     }
 }
