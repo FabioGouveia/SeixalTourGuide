487efd566a8a05cccfe20f3ce94cfbc7a284cf62
Fábio Gouveia <hpgroods@gmail.com>
2018-05-07 00:59:11 +0100

    feat: add title parameter to Attraction constructor method
    Signed-off-by: Fábio Gouveia <hpgroods@gmail.com>

--- app/src/main/java/com/example/android/seixaltourguide/model/Attraction.java
+++ app/src/main/java/com/example/android/seixaltourguide/model/Attraction.java
@@ -2,7 +2,8 @@ package com.example.android.seixaltourguide.model;
 
 public class Attraction extends Place {
 
-    public Attraction(String description, String address){
+    public Attraction(String title, String description, String address) {
+        this.title = title;
         this.description = description;
         this.address = address;
     }
