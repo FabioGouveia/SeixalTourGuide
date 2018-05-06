4ba7415595876a69e52b728f92bc3ec649331117
Fábio Gouveia <hpgroods@gmail.com>
2018-05-07 00:57:27 +0100

    feat: add restaurant model
    Signed-off-by: Fábio Gouveia <hpgroods@gmail.com>

---
+++ app/src/main/java/com/example/android/seixaltourguide/model/Restaurant.java
@@ -0,0 +1,12 @@
+package com.example.android.seixaltourguide.model;
+
+
+public class Restaurant extends Place {
+
+    public Restaurant(String title, String description, String address) {
+        this.title = title;
+        this.description = description;
+        this.address = address;
+    }
+
+}
