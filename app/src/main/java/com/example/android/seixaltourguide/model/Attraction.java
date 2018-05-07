31fdbfd4fb23c2b662adc530c3d6710b4d58a1af
Fábio Gouveia <hpgroods@gmail.com>
2018-05-07 01:23:05 +0100

    fix: fix constructor to match super class
    Signed-off-by: Fábio Gouveia <hpgroods@gmail.com>

--- app/src/main/java/com/example/android/seixaltourguide/model/Attraction.java
+++ app/src/main/java/com/example/android/seixaltourguide/model/Attraction.java
@@ -3,8 +3,6 @@ package com.example.android.seixaltourguide.model;
 public class Attraction extends Place {
 
     public Attraction(String title, String description, String address) {
-        this.title = title;
-        this.description = description;
-        this.address = address;
+        super(title, description, address);
     }
 }
